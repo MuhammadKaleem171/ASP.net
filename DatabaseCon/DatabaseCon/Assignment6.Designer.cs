bfe75f9b60e82250f51f05334fc8086b8f9271e5
Malik_Kaleem <malikkaleem171@gmail.com>
2020-04-08 15:24:51 +0500

    Ass 6 question 1

---
+++ DatabaseCon/DatabaseCon/Assignment6.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DatabaseCon
+{
+    partial class Assignment6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemovr = new System.Windows.Forms.Button();
+            this.btnSelected = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(270, 108);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "ADd";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemovr
+            // 
+            this.btnRemovr.Location = new System.Drawing.Point(378, 108);
+            this.btnRemovr.Name = "btnRemovr";
+            this.btnRemovr.Size = new System.Drawing.Size(75, 23);
+            this.btnRemovr.TabIndex = 3;
+            this.btnRemovr.Text = "button2";
+            this.btnRemovr.UseVisualStyleBackColor = true;
+            this.btnRemovr.Click += new System.EventHandler(this.btnRemovr_Click);
+            // 
+            // btnSelected
+            // 
+            this.btnSelected.Location = new System.Drawing.Point(86, 344);
+            this.btnSelected.Name = "btnSelected";
+            this.btnSelected.Size = new System.Drawing.Size(75, 23);
+            this.btnSelected.TabIndex = 4;
+            this.btnSelected.Text = "button3";
+            this.btnSelected.UseVisualStyleBackColor = true;
+            this.btnSelected.Click += new System.EventHandler(this.btnSelected_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(240, 344);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "button4";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(420, 344);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "button5";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Assignment6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.btnSelected);
+            this.Controls.Add(this.btnRemovr);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Assignment6";
+            this.Text = "Assignment6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemovr;
+        private System.Windows.Forms.Button btnSelected;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+    }
+}
---
+++ DatabaseCon/DatabaseCon/Assignment6.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseCon
+{
+    public partial class Assignment6 : Form
+    {
+        public Assignment6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            listBox1.Items.Add(item);
+        }
+
+        private void btnRemovr_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+
+            int c = check(item);
+            if(c != -1)
+            {
+                listBox1.Items.RemoveAt(c);
+            }
+            else
+            {
+                MessageBox.Show("not found");
+            }
+           }
+        int check(string item)
+        {
+            int c = 0; bool f = false;
+            foreach (string items in listBox1.Items)
+            {
+                if (item == items)
+                {
+                    f = true;
+                    break;
+                }
+                c++;
+            }
+            if (f == true)
+                return c;
+            else return -1;
+        }
+
+        private void btnSelected_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(index);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
